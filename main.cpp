d5d9bfbf7228f2ce561e9225d54c3e207fa42414
omar-a-007 <oalku001@ucr.edu>
2021-01-14 17:20:30 -0800

    Modify main to output count of strings passed in.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <string>
+#include "c-echo.h"
 
-int main()
+
+int main(int argv, char** argc)
 {
-  std::string name;
-  std::cout << "What is your name?" << std::endl;
-  std::cin >> name;
-  std::cout << "Hello " << name << "!" << std::endl;
+     std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 
   return 0;
 }
